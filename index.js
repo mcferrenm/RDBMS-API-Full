ec84a137fe5785b80acc72c523c7dc7c65895d77
mcferrenm <mcferrenmax@gmail.com>
2019-02-20 15:43:04 -0500

    Express boilerplate

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const server = express();
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+  console.log(`\n\n*** Now listening on port ${PORT} ***\n`)
+})
