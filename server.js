02f10a17e500997ee6c7436239ba5fba7f60ecbb
mcferrenm <mcferrenmax@gmail.com>
2019-02-20 17:17:10 -0500

    Add cohorts CRUD API

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const knex = require("knex");
 
-const knexConfig = require("./knexfile");
 const cohortsRouter = require('./cohorts/cohorts-router')
 const studentsRouter = require('./students/students-router')
 
-const db = knex(knexConfig);
 const server = express();
 
+// Global Middleware
+server.use(express.json())
+
+// Routes
 server.use('/api/cohorts', cohortsRouter);
 server.use('/api/students', studentsRouter);
 
--- students/students-router.js
+++ students/students-router.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const knex = require("knex");
 
+const knexConfig = require("../knexfile");
+
+const db = knex(knexConfig);
 const router = express.Router();
 
 router.get("/", (req, res) => {
